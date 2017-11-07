473e3268bbcac2f146bbb589d14e2a2faf5b7561
sharknado5 <loukoekemoer@gmail.com>
2017-11-07 15:03:42 +0200

    Grace_kelly

--- 2_grace/Grace.c
+++ 2_grace/Grace.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
+/*da_comment*/
 #define A(a) void func1(){}
 #define B(b) void func2(){}
-#define FT(x) int main() {FILE *fp = fopen("Grace_kid.c", "w"); char *s = "%c/*da_comment*/%c#include <stdio.h>%c#define A(a) void func1(){}%c#define B(b) void func2(){}%c#define FT(x)%cint main() {%cchar* s = %c%s%c;%cFILE *fp = fopen(%cGrace_kid.c%c, %cw%c);%cfprintf(fp, s, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 34, 34, 34, 34,10, 10, 10, 10);%cfclose(fp);%c}%c";fprintf(fp, s, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 34, 34, 34, 34, 10, 10, 10, 10);fclose(fp);}
+#define FT(x) int main() {FILE *fp = fopen("Grace_kid.c", "w");char *s = "#include <stdio.h>%c/*da_comment*/%c#define A(a) void func1(){}%c#define B(b) void func2(){}%c#define FT(x) int main() {FILE *fp = fopen(%cGrace_kid.c%c, %cw%c);char *s = %c%s%c;fprintf(fp, s, 10, 10, 10, 10, 34, 34, 34, 34, 34, s, 34, 10, 10);fclose(fp);}%cFT()%c";fprintf(fp, s, 10, 10, 10, 10, 34, 34, 34, 34, 34, s, 34, 10, 10);fclose(fp);}
 FT()
--- 2_grace/comp.sh
+++ 2_grace/comp.sh
@@ -1,3 +1,5 @@
 make re
 ./Grace
+cat Grace.c
 cat Grace_kid.c
+diff Grace.c Grace_kid.c
