9bfceb269baa7471ebbd1ff6b0d75e9283afe056
sharknado5 <loukoekemoer@gmail.com>
2017-11-10 18:22:23 +0200

    redo

--- 3_sully/Sully.c
+++ 3_sully/Sully.c
@@ -1,4 +1,7 @@
-/* SULLY */
+/* SULLY - Polyglot Quine 
+ When executed the program writes in a file named Sully_X.c. The X will be an
+ interger given in the source. Once the file is created, the program compiles this file
+ and then runs the new program (which will have the name of its source file). */
 #include <stdio.h>
 #include <stdlib.h>
 #define D 34
@@ -13,9 +16,10 @@ int main()
 	char *s2 = "int main()%c{%cFILE *fp;%cchar name[10] = {%cS%c,%cu%c,%cl%c,%cl%c,%cy%c,%c_%c,%cX%c,%c.%c,%cc%c};%c";
 	char *s3 = "char *s1 = %c%s%c;%cchar *s2 = %c%s%c;%cchar *s3 = %c%s%c;%cchar *s4 = %c%s%c;%cchar i = 5;%cwhile (i > 0){%cname[6] = i + %c0%c;%cfp = fopen(name, %cw%c);%c";
 	char *s4 = "fprintf(fp,s1,N,N,N,N,N,N,N);%cfprintf(fp,s2,N,N,N,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,N);%cfprintf(fp,s3,D,s1,D,N,D,s2,D,N,D,s3,D,N,D,s4,D,N,N,N,S,S,N,D,D,N);%ci--;%cfclose(fp);%c}%creturn 0;%c}%c";
-//	char *s3 = "char *s3 = %s;%c";//%cwhile (i > 0){%cname[6] = i + '0';%c";//fp = fopen(name, %cw%c);%c";
-//	char *comp = "gcc -Wall -Werror -Wextra ";
 	char i = 5;
+	char test[500];
+	sprintf(test, "BLABLABALBALBALBABLABLABLBLABLABLABLA\n");
+	printf("%s", test);
 	while (i > 0){
 		name[6] = i + '0';
 		fp = fopen(name, "w");
@@ -23,10 +27,8 @@ int main()
 		fprintf(fp,s2,N,N,N,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,N);
 		fprintf(fp,s3,D,s1,D,N,D,s2,D,N,D,s3,D,N,D,s4,D,N,N,N,S,S,N,D,D,N);
 		fprintf(fp,s4,N,N,N,N,N,N,N);
-		//printf("%s%s",comp,name);
-		//fprintf(0,"gcc Sully_1.c -o test");
-		i--;
 		fclose(fp);
+		i--;
 	}
 	return 0;
 }
